0d784e0da7aa98cf51195a6983581df30c96e071
JackyPHO <100171161+JackyPHO@users.noreply.github.com>
2025-04-07 20:15:30 -0700

    ex1v1

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -1,34 +1,47 @@
-// project.js - purpose and description here
-// Author: Your Name
-// Date:
-
-// NOTE: This is how we might start a basic JavaaScript OOP project
-
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-
-// define a class
-class MyProjectClass {
-  // constructor function
-  constructor(param1, param2) {
-    // set properties using 'this' keyword
-    this.property1 = param1;
-    this.property2 = param2;
-  }
+const fillers = {
+  places: ["Disneyland", "New York City", "Paris", "Tokyo"],
+  season: ["summer", "spring", "fall", "winter"],
+  people: ["famiy", "significant other", "no one"],
+  food: ["burritos", "burgers", "noodles", "sushi", "steak"], 
+  stay: ["five star hotel", "beach house", "cabin", "dirty motel", "relative's home"],
+  number: ["1","2","3","4","5"],
   
-  // define a method
-  myMethod() {
-    // code to run when method is called
+};
+
+const template = `During the $season time,
+
+you will spend a nice vacation at $places with $people for $number days. 
+
+When the sun goes down, you will enjoy a nice dinner eating $food and spend the rest of the night at $stay.
+
+Hope you enjoy your vacation!
+`;
+
+
+// STUDENTS: You don't need to edit code below this line.
+
+const slotPattern = /\$(\w+)/;
+
+function replacer(match, name) {
+  let options = fillers[name];
+  if (options) {
+    return options[Math.floor(Math.random() * options.length)];
+  } else {
+    return `<UNKNOWN:${name}>`;
   }
 }
 
-function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
+function generate() {
+  let story = template;
+  while (story.match(slotPattern)) {
+    story = story.replace(slotPattern, replacer);
+  }
 
-  // call a method on the instance
-  myInstance.myMethod();
+  /* global box */
+  $("#box").text(story);
 }
 
-// let's get this party started - uncomment me
-//main();
+/* global clicker */
+$("#clicker").click(generate);
+
+generate();
